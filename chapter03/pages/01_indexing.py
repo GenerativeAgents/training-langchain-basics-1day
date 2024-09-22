cbb53867afea649c8c491d5fc0040f9f4e929532
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2024-09-22 20:44:14 +0900

    Add chapter03 apps

--- chapter03/pages/01_indexing.py
+++ chapter03/pages/01_indexing.py
@@ -1,3 +1,44 @@
 import streamlit as st
+from dotenv import load_dotenv
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import GitLoader
+from langchain_openai import OpenAIEmbeddings
 
-st.title("Indexing")
+
+def app() -> None:
+    load_dotenv(override=True)
+
+    st.title("Indexing")
+
+    clicked = st.button("実行")
+    if not clicked:
+        return
+
+    def file_filter(file_path: str) -> bool:
+        return file_path.endswith(".mdx")
+
+    loader = GitLoader(
+        clone_url="https://github.com/langchain-ai/langchain",
+        repo_path="./tmp/langchain",
+        branch="master",
+        file_filter=file_filter,
+    )
+
+    with st.spinner("Loading documents..."):
+        documents = loader.load()
+
+    st.info(f"{len(documents)} documents loaded.")
+
+    embedding = OpenAIEmbeddings(model="text-embedding-3-small")
+
+    with st.spinner("Indexing documents..."):
+        Chroma.from_documents(
+            documents=documents,
+            embedding=embedding,
+            persist_directory="./tmp/chroma",
+        )
+
+    st.success("Indexing completed.")
+
+
+app()
--- chapter03/pages/02_rag.py
+++ chapter03/pages/02_rag.py
@@ -1,3 +1,66 @@
-import streamlit as st
+from typing import Any
 
-st.title("RAG")
+import streamlit as st
+from dotenv import load_dotenv
+from langchain_chroma import Chroma
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import Runnable, RunnablePassthrough
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
+_prompt_template = '''
+以下の文脈だけを踏まえて質問に回答してください。
+
+文脈: """
+{context}
+"""
+
+質問: {question}
+'''
+
+
+def create_rag_chain() -> Runnable[Any, str]:
+    embedding = OpenAIEmbeddings(model="text-embedding-3-small")
+    vector_store = Chroma(
+        embedding_function=embedding,
+        persist_directory="./tmp/chroma",
+    )
+
+    retriever = vector_store.as_retriever()
+    prompt = ChatPromptTemplate.from_template(_prompt_template)
+
+    model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+    return (
+        {
+            "context": retriever,
+            "question": RunnablePassthrough(),
+        }
+        | prompt
+        | model
+        | StrOutputParser()
+    )
+
+
+def app() -> None:
+    load_dotenv(override=True)
+
+    st.title("RAG")
+
+    # Chainを初期化
+    if "chain" not in st.session_state:
+        st.session_state.chain = create_rag_chain()
+    chain = st.session_state.chain
+
+    # ユーザーの質問を受け付ける
+    question = st.text_input("質問を入力してください")
+    if not question:
+        return
+
+    # 回答を生成して表示
+    stream = chain.stream(question)
+    with st.spinner():
+        st.write_stream(stream)
+
+
+app()
